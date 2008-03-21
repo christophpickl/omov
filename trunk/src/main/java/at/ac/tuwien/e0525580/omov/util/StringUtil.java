73b0894912ad699e4b7b1f93414e9f0b4e48d4b0
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-03-21 19:45:34 +0000

    commented needleman string similarty method out

--- trunk/src/main/java/at/ac/tuwien/e0525580/omov/util/StringUtil.java
+++ trunk/src/main/java/at/ac/tuwien/e0525580/omov/util/StringUtil.java
@@ -2,16 +2,9 @@ package at.ac.tuwien.e0525580.omov.util;
 
 import java.io.File;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import at.ac.tuwien.e0525580.omov.spielwiese.StringSimilarity;
-
-import uk.ac.shef.wit.simmetrics.similaritymetrics.NeedlemanWunch;
-
 public class StringUtil {
 
-    private static final Log LOG = LogFactory.getLog(StringUtil.class);
+//    private static final Log LOG = LogFactory.getLog(StringUtil.class);
     
     private StringUtil() {
         // no instantiation
@@ -34,13 +27,13 @@ public class StringUtil {
         return withLineFeeds.replaceAll("\n", "\\\\n");
     }
     
-    private static final NeedlemanWunch NEEDLEMAN_WUNCH = new NeedlemanWunch();
-    /**
-     * @see StringSimilarity#needleman()
-     */
-    public static float similarity(String s1, String s2) {
-        final float similarity = NEEDLEMAN_WUNCH.getSimilarity(s1, s2);
-        LOG.debug("returning similarity of '"+similarity+"' for string s1 '"+s1+"' and s2 '"+s2+"'.");
-        return similarity;
-    }
+//    private static final NeedlemanWunch NEEDLEMAN_WUNCH = new NeedlemanWunch();
+//    /**
+//     * @see StringSimilarity#needleman()
+//     */
+//    public static float similarity(String s1, String s2) {
+//        final float similarity = NEEDLEMAN_WUNCH.getSimilarity(s1, s2);
+//        LOG.debug("returning similarity of '"+similarity+"' for string s1 '"+s1+"' and s2 '"+s2+"'.");
+//        return similarity;
+//    }
 }
