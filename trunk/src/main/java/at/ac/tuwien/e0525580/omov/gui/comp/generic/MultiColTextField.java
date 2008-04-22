53276d465a75bf8cc43b2d0326507294037372eb
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-04-22 16:48:19 +0000

    fixed bug concerning empty tooltips

--- trunk/src/main/java/at/ac/tuwien/e0525580/omov/gui/comp/generic/MultiColTextField.java
+++ trunk/src/main/java/at/ac/tuwien/e0525580/omov/gui/comp/generic/MultiColTextField.java
@@ -6,12 +6,9 @@ import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class MultiColTextField extends JLabel {
 
-    private static final Log LOG = LogFactory.getLog(MultiColTextField.class);
+//    private static final Log LOG = LogFactory.getLog(MultiColTextField.class);
     private static final long serialVersionUID = 2860809976529219917L;
     
     private final int visibleTextColumns;
@@ -30,14 +27,12 @@ public class MultiColTextField extends JLabel {
         
     	
 //        this.setOpaque(true); this.setBackground(Color.RED);
-//    	this.setOpaque(false);
+    	this.setOpaque(false);
         
         this.setHorizontalAlignment(JLabel.LEFT);
         this.setVerticalAlignment(JLabel.TOP);
         
-//    	this.setBackground(null);
         this.setBorder(BorderFactory.createEmptyBorder());
-//        this.setEditable(false);
         
         this.setText(text);
     }
@@ -46,16 +41,21 @@ public class MultiColTextField extends JLabel {
     public void setText(String text) {
     	this.realText = text;
     	
+    	
         final String limitedText;
         if(text.length() > this.visibleTextColumns) {
         	limitedText = text.substring(0, this.visibleTextColumns) + "...";
         } else {
         	limitedText = text;
         }
-        LOG.debug("Setting text to '"+limitedText+"' and (visibleTextColumns="+visibleTextColumns+") tooltip to '"+text+"'.");
+//        LOG.debug("Setting text to '"+limitedText+"' and (visibleTextColumns="+visibleTextColumns+") tooltip to '"+text+"'.");
         super.setText(limitedText);
-        this.setToolTipText(text);
-//        this.setCaretPosition(0);
+        
+        if(text.length() == 0) {
+        	this.setToolTipText(null);
+        } else {
+        	this.setToolTipText(text);
+        }
     }
     
     public String getRealText() {
