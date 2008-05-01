bd45dc55f0e831b226a682647efaf9020a845e7c
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-01 18:32:23 +0000

    total reorganization of structure

--- tags/before-modularization/src/main/java/at/ac/tuwien/e0525580/omov/gui/comp/generic/table/IColumnComparable.java
+++
@@ -1,6 +0,0 @@
-package at.ac.tuwien.e0525580.omov.gui.comp.generic.table;
-
-public interface IColumnComparable<T> {
-
-    abstract int compareValue(T m1, T m2);
-}
--- tags/release-0.3/src/main/java/at/ac/tuwien/e0525580/omov/gui/comp/intime/IPrefilledIntimeList.java
+++
@@ -1,7 +0,0 @@
-package at.ac.tuwien.e0525580.omov.gui.comp.intime;
-
-public interface IPrefilledIntimeList {
-
-    void unregisterIntimeDatabaseModel();
-    
-}
