0f95e5241c864d655348bb208acb992dae282a4c
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-01 19:05:50 +0000

    Initial import of reorganized structure.

---
+++ omovQtjImpl/trunk/src/main/java/net/sourceforge/omov/qtjImpl/SessionInitializer.java
@@ -0,0 +1,40 @@
+package net.sourceforge.omov.qtjImpl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import quicktime.QTException;
+import quicktime.QTSession;
+
+
+class SessionInitializer {
+	
+    private static final Log LOG = LogFactory.getLog(SessionInitializer.class);
+
+	private static boolean isOpened = false;
+	
+	public static void openSession() throws QTException {
+		
+		if(isOpened == false) {
+			LOG.info("Opening quicktime session...");
+			QTSession.open();
+			LOG.info("QT version: " + QTSession.getMajorVersion() + "." + QTSession.getMinorVersion());
+			// TODO check for qt version > 7 ?
+			
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				public void run() {
+					LOG.info("Closing QuickTime session...");
+					try {
+						QTSession.close();
+					} catch(Exception e) {
+						LOG.error("Could not close QuickTime session!", e);
+	                    e.printStackTrace();
+					}
+				}
+			}));
+			
+			isOpened = true;
+		}
+	}
+	
+}
