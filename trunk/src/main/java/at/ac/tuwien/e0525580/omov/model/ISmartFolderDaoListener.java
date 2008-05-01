e5f159718603e0cfa6026cf8e5d40a03d81190f2
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-01 18:31:48 +0000

    total reorganization of structure

--- trunk/src/main/java/at/ac/tuwien/e0525580/omov/model/ISmartFolderDaoListener.java
+++
@@ -1,7 +0,0 @@
-package at.ac.tuwien.e0525580.omov.model;
-
-public interface ISmartFolderDaoListener {
-
-    void smartFolderDataChanged();
-    
-}
--- trunk/src/main/java/at/ac/tuwien/e0525580/omov/tools/export/IExporterHtml.java
+++
@@ -1,9 +0,0 @@
-package at.ac.tuwien.e0525580.omov.tools.export;
-
-import java.io.File;
-
-interface IExporterHtml {
-
-    File getCoversFolder();
-    
-}
