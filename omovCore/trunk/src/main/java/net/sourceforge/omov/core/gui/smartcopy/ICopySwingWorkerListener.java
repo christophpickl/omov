0939451075f643cebefe53552ae5f1b8d5ecb74a
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-02 22:49:02 +0000

    2nd reorganization (omovApp).

--- omovCore/trunk/src/main/java/net/sourceforge/omov/core/gui/smartcopy/ICopySwingWorkerListener.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.omov.core.gui.smartcopy;
-
-public interface ICopySwingWorkerListener {
-
-    void workerFinished(boolean wasAborted);
-//    void workerFinished();
-    
-}
