14c76a3a7e4cbc4e16c68fbba1c7d7b07430d052
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-02 22:54:31 +0000

    2nd reorganization (omovApp).

--- omovCore/trunk/src/main/java/net/sourceforge/omov/core/gui/comp/generic/ButtonPosition.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.omov.core.gui.comp.generic;
-
-/**
- * used by Directory- and FileChooser (actually by AbstractFileDirectoryChooser)
- */
-public enum ButtonPosition {
-    LEFT, RIGHT;
-}
--- omovCore/trunk/src/main/java/net/sourceforge/omov/core/gui/comp/generic/table/IColumnComparable.java
+++
@@ -1,6 +0,0 @@
-package net.sourceforge.omov.core.gui.comp.generic.table;
-
-public interface IColumnComparable<T> {
-
-    abstract int compareValue(T m1, T m2);
-}
