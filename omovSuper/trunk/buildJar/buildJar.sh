4a9fb74162a226f0501966dba69bf2ae12aad6b9
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-19 21:34:27 +0000

    finished deploy of v0.5

--- omovSuper/trunk/buildJar/buildJar.sh
+++ omovSuper/trunk/buildJar/buildJar.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+######################
+# 
+# OurMovies - Yet another movie manager
+# Copyright (C) 2008 Christoph Pickl (christoph_pickl@users.sourceforge.net)
+# http://omov.sourceforge.net
+# 
+################################################################
+
 cd ..
+
+cd ../omovSuper
+# all submodules will be cleaned + compiled + tested + installed2repo
+mvn clean install
+
 cd ../omovCore
-mvn clean install
-cd ../omovQtjApi
-mvn clean install
-cd ../omovQtjImpl
-mvn clean install
-cd ../omovCore
-mvn -Dmaven.test.skip=true assembly:assembly
+#mvn -Dmaven.test.skip=true assembly:assembly
+mvn assembly:assembly
+
