b793c5c84d364d61b307ab8f393f968c5a90b917
christoph_pickl <christoph_pickl@8d345ef9-1247-0410-922b-db90f5dccd91>
2008-05-19 21:58:36 +0000

    v0-5

---
+++ omovWebApi/tags/v0-5/src/main/java/net/sourceforge/omov/webApi/IWebDataFetcher.java
@@ -0,0 +1,29 @@
+package net.sourceforge.omov.webApi;
+
+import java.util.List;
+
+import net.sourceforge.omov.core.BusinessException;
+import net.sourceforge.omov.core.bo.Movie;
+
+
+//FEATURE websearch: maybe automatically check HOST + "/robots.txt" for permission?!
+
+/**
+* 
+* @author christoph_pickl@users.sourceforge.net
+*/
+public interface IWebDataFetcher {
+	
+    List<WebSearchResult> search(String movieTitle) throws BusinessException;
+    
+    Movie getDetails(WebSearchResult searchResult, boolean fetchCover) throws BusinessException;
+    
+//    Movie enhanceMovie(Movie originalMovie, Movie metadataMovie);
+    
+    /**
+     * invokes this.search(movie.getTitle()) and uses first WebSearchResult to enhance the given movie.
+     * 
+     * @return null if nothing was found.
+     */
+    Movie fetchAndEnhanceMovie(Movie originalMovie, boolean fetchCover) throws BusinessException;
+}
---
+++ omovWebApi/tags/v0-5/src/main/java/net/sourceforge/omov/webApi/WebSearchResult.java
@@ -0,0 +1,46 @@
+package net.sourceforge.omov.webApi;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+
+/**
+ * Value class, storing results of websearch (by movie title).
+ * 
+ * @author christoph_pickl@users.sourceforge.net
+ */
+public class WebSearchResult { // TODO rename class to something like "WebSearchTarget"
+    
+    private final String label;
+    
+    private final String url;
+    
+    
+    public WebSearchResult(final String label, final String url) {
+        this.label = StringEscapeUtils.unescapeHtml(label);
+        this.url = url;
+    }
+    
+    public String toString() {
+        return "WebSearchResult[label="+label+";url="+url+"]";
+    }
+    
+    public boolean equals(Object object) {
+        if((object instanceof WebSearchResult) == false) return false;
+        WebSearchResult that = (WebSearchResult) object;
+        return this.url.equals(that.url);
+    }
+    
+    public int hashCode() {
+        return this.url.hashCode();
+    }
+    
+    public String getLabel() {
+        return this.label;
+    }
+    
+    public String getUrl() {
+        return this.url;
+    }
+    
+    
+}
